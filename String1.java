d6499bb78415cbbfc00e817fa2b2d051856b116e
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-04 21:45:44 -0700

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,15 @@
+package programs;
+import java.util.Scanner;
+public class String1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter string : ");
+		String s=sc.nextLine();
+		System.out.println("The Character At Position 0 is "+s.charAt(0));
+		System.out.println("The Character At Position 9 is "+s.charAt(9));
+		
+
+	}
+
+}
