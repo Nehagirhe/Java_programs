140e5bee49fdf7372115ad142e38532f2330054b
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-12 20:33:05 -0700

    Add files via upload

---
+++ charcters.java
@@ -0,0 +1,11 @@
+
+public class charcters {
+
+	public static void main(String[] args) {
+		String s="Asteriscjava";
+		System.out.println(s.charAt(0));
+		System.out.println(s.charAt(9));
+
+	}
+
+}
