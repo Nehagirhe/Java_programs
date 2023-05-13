6d1b90dbd43cbdf3a0913c9c7d9952257d505a33
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-12 20:36:05 -0700

    Update charcters.java

--- charcters.java
+++ charcters.java
@@ -1,10 +1,11 @@
+//Java Program To Get The Character At The Given Index Within The String.
 
-public class charcters {
-
+public class charcters
+{	
 	public static void main(String[] args) {
-		String s="Asteriscjava";
-		System.out.println(s.charAt(0));
-		System.out.println(s.charAt(9));
+	String s="Asteriscjava";
+	System.out.println(s.charAt(0));
+	System.out.println(s.charAt(9));
 
 	}
 
