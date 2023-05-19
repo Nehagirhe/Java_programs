d4b49922b2c60afa16e694670ba7452ee8001fe9
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-18 20:43:49 -0700

    Add files via upload

---
+++ Classes And Objects/Class4.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class Class4 {
+double len,breadth;
+
+	Class4(double l,double b)
+	{
+	len=l;
+	breadth=b;
+	
+	}
+	
+	double Area()
+	{
+		return len*breadth;
+	}
+	
+}
