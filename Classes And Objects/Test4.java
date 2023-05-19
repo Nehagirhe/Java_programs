79e836eb7c9a5d096096d0a9e4b11e39f8c36af3
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-18 20:44:35 -0700

    Add files via upload

---
+++ Classes And Objects/Test4.java
@@ -0,0 +1,10 @@
+package classes;
+public class Test4 {
+	public static void main(String[] args) {
+		Class4 c=new Class4(4,5);
+		Class4 c1=new Class4(5,8);
+		System.out.println("Area : "+c.Area());
+		System.out.println("Area1 : "+c1.Area()); 
+	}
+
+}
