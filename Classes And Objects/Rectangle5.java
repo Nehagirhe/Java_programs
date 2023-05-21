d6263e11b2284008f3f9c25693e39d95c4a63909
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-21 08:00:01 -0700

    Add files via upload

---
+++ Classes And Objects/Rectangle5.java
@@ -0,0 +1,18 @@
+package classes;
+import java.util.Scanner;
+public class Rectangle5 
+{
+	int length,breadth,area;
+Scanner sc= new Scanner(System.in);
+Rectangle5(int l,int b)
+{
+ length=l;
+breadth=b;
+}
+int Area()
+{
+	 area=length*breadth;
+	return area;
+}
+
+}
