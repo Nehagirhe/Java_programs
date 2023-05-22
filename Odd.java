1c643048d6ec956f7a177a77b5f06747ea59ec8f
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-22 07:47:35 -0700

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,19 @@
+package pracprog;
+import java.util.Scanner;
+public class Odd {
+
+	public static void main(String[] args) 
+	{	
+     Scanner sc=new Scanner(System.in);
+     System.out.println("::ODD NUMBERS::");
+     for(int i=0;i<50;i++)
+     {
+      if(i%2!=0)
+      {
+    	  System.out.println(i);
+      }
+     
+     }
+	}
+
+}
