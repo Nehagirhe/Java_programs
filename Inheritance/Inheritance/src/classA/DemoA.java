f5e5014c6d7ef32108561ec60df84bbf84e0d1b8
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-25 01:16:09 -0700

    Add files via upload

---
+++ Inheritance/Inheritance/src/classA/DemoA.java
@@ -0,0 +1,12 @@
+package classA;
+
+public class DemoA {
+	int area;
+	int Rectangle(int l,int b)
+	{
+		area=l*b;
+		System.out.println("Area Of Rectangle :"+area);
+		return area;
+	}
+	
+}
---
+++ Inheritance/Inheritance/src/classA/DemoB.java
@@ -0,0 +1,11 @@
+package classA;
+
+public class DemoB extends DemoA{
+	int Square(int s)
+	{
+		area=s*s;
+		return area;
+	}
+	
+
+}
