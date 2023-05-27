e1963d9591d098a02a811c65d5422fef48990813
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-27 03:58:44 -0700

    Update DemoA.java

--- Inheritance/Inheritance/src/classA/DemoA.java
+++ Inheritance/Inheritance/src/classA/DemoA.java
@@ -1,9 +1,11 @@
 package classA;
 
-public class DemoA {
+public class DemoA 
+{
 	int area;
 	int Rectangle(int l,int b)
 	{
+		
 		area=l*b;
 		System.out.println("Area Of Rectangle :"+area);
 		return area;
