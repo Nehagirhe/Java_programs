18d2a22cba747586e6b17b3865f5bfd587e6145c
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-28 04:28:57 -0700

    Update Loop.java

--- loop/Loop.java
+++ loop/Loop.java
@@ -1,9 +1,10 @@
 package pracprog;
 import java.util.Scanner;
-public class Loop {
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+public class Loop
+{
+	public static void main(String[] args) 
+	{
+	Scanner sc=new Scanner(System.in);
     int arr[]= {1,2,3,4,5};
 	for(int x:arr)
 	{
