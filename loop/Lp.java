3f75ac687b3038e14422cc34cc49b5f26ac25bc9
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-30 04:57:45 -0700

    Add files via upload
    Program To Calculate Cubes

---
+++ loop/Lp.java
@@ -0,0 +1,20 @@
+package pracprog;
+import java.util.Scanner;
+public class Lp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+System.out.println("Enter How Many Values To Calculate Cube :");
+int n=Integer.parseInt(sc.nextLine());
+for(int i=1;i<=n;i++)
+{
+	System.out.print("Number is "+i);
+	System.out.print(" And Cube Of The "+i);
+	//int x=i*i*i;
+	System.out.println(" Is : "+i*i*i);
+}
+
+	}
+
+}
