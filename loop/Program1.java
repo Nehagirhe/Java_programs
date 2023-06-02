cbd2ef405c9560249d8a93d73e4691ba881778de
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-06-02 02:38:58 -0700

    Add files via upload

---
+++ loop/Program1.java
@@ -0,0 +1,27 @@
+/* Program For Print 
+*****
+****
+***
+**
+*
+ */
+
+
+
+package pattern_programs;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+	for(int i=0;i<5;i++)
+	{
+		for(int j=5;j>i;j--)
+		{
+			System.out.print("*");
+		}
+		System.out.print("\n");
+	}
+	
+	}
+
+}
