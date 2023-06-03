67b97c2090874c86bfdfed03c5f73d4bab5166d8
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-06-03 04:32:24 -0700

    Add files via upload

---
+++ Pattern_programs/Pattern.java
@@ -0,0 +1,19 @@
+package pracprog;
+
+public class Pattern {
+
+	public static void main(String[] args) 
+	{
+	for(int i=1;i<5;i++)
+	{
+		for(int j=5;j>i;j--)
+		{
+			System.out.print(i);
+		}
+		System.out.print("\n");
+	}
+	
+
+	}
+
+}
