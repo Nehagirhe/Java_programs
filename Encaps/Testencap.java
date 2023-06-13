c47419eacb76a3709f09b3e462fa4e12d3c0d5ae
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-06-13 04:35:35 -0700

    Add files via upload

---
+++ Encaps/Testencap.java
@@ -0,0 +1,18 @@
+package neha;
+
+public class Testencap {
+
+	public static void main(String[] args) {
+		
+		Account acc= new Account();
+		acc.setAccno(675548976);
+		acc.setName("Neha Girhe");
+		acc.setEmail("nehagirhe23@gmail.com");
+		acc.setAmount(25000f);
+		System.out.println("Account Number :  "+acc.getAccno());
+	    System.out.println("Name : "+acc.getName());
+		System.out.println("Email : "+acc.getEmail());
+        System.out.println("Amount : "+acc.getAmount());
+	}
+
+}
