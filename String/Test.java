dbcf28b30b9642e38319b273abb712127a1112a7
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-06-19 08:46:46 +0530

    Update Test.java

--- String/Test.java
+++ String/Test.java
@@ -11,13 +11,13 @@ public class Test {
 	String s4=new String("hello");       //2 memory heap/scp
 	System.out.println(s==s1);           //compare object  
 	System.out.println(s.equals(s1));    //compare data
-	System.out.println(s1==s2);          //true
-    System.out.println(s==s3);           //false
-    System.out.println(s1==s3);          //false
-    System.out.println(s.equals(s3));    //false
-    System.out.println(s1.equals(s3));   //false
-    System.out.println(s==s4);           //false
-	System.out.println(s.equals(s4));    //true
+	System.out.println(s1==s2);            //true
+    System.out.println(s==s3);                //false
+    System.out.println(s1==s3);              //false
+    System.out.println(s.equals(s3));       //false
+    System.out.println(s1.equals(s3));     //false
+    System.out.println(s==s4);            //false
+	System.out.println(s.equals(s4)); //true
 	}
 
 }
