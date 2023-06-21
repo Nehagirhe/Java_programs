7a3e7d76bce765780ef075e5a7778c657e7ee6a2
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-06-21 09:31:16 +0530

    Add files via upload

---
+++ Abstraction/Test.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public class Test {
+
+	public static void main(String[] args) {
+		HeroElectricZoin H= new HeroElectricZoin();
+		H.BikeType();
+		H.Brand();
+		H.colour();
+		H.Size();
+		H.Wheeldiameter();
+	}
+
+}
