80ffbf1a8cc20dd4bd8577e627e765503161d10c
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-06-24 07:40:49 +0530

    Add files via upload

---
+++ Interface/Print.java
@@ -0,0 +1,20 @@
+package Interface;
+
+interface print
+{  
+void print();
+}
+
+class A6 implements print
+{  
+public void print()
+{
+System.out.println("Hello");
+}  
+		  
+public static void main(String args[])
+{  
+A6 obj = new A6();  
+obj.print();
+}  
+}
