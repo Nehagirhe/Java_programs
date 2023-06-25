002e9fd426ba31b2c06ba938d6928619229966a2
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-06-25 17:38:37 +0530

    Update Testencap.java

--- Encaps/Testencap.java
+++ Encaps/Testencap.java
@@ -3,10 +3,10 @@ package neha;
 public class Testencap {
 
 	public static void main(String[] args) {
-		
 		Account acc= new Account();
 		acc.setAccno(675548976);
 		acc.setName("Neha Girhe");
+		
 		acc.setEmail("nehagirhe23@gmail.com");
 		acc.setAmount(25000f);
 		System.out.println("Account Number :  "+acc.getAccno());
