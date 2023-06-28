afd508626bf0054f306f0289f1320defaf6860ab
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-06-28 08:54:08 +0530

    Add files via upload

---
+++ Array/Num.java
@@ -0,0 +1,14 @@
+
+public class Num {
+	
+	public static void main(String[] args)
+	{
+     int a[]=new int [] {2,3,4,5,6,9};
+System.out.println("Number Of Elements : "+a.length); 
+
+String s[]=new String[] {"Neha","Nikita","Tina","Pinu"};
+
+System.out.println("Number Of Elements In The String : "+s.length);
+	}
+
+}
