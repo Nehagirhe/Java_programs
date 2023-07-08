e5847d6a066d8bb0c20a11a24b6e886e20990cb4
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-07-08 08:59:17 +0530

    Add files via upload

---
+++ Collection/ArrayList/IncreaseSize.java
@@ -0,0 +1,19 @@
+package arrList;
+
+import java.util.ArrayList;
+
+public class IncreaseSize {
+
+public static void main(String[] args) {
+ArrayList <String>a=new ArrayList<String>();
+a.add("Red");
+a.add("Green");
+a.add("Black");
+System.out.println("Original Array List: "+a);
+a.add("White");
+a.add("Pink");
+a.add("Yellow");
+System.out.println(a);
+}
+
+}
