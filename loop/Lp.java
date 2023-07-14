c492da44d05d43b011126c20c53dae30f683e63e
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-07-14 15:21:25 +0530

    Update Lp.java

--- loop/Lp.java
+++ loop/Lp.java
@@ -1,20 +1,19 @@
 package pracprog;
 import java.util.Scanner;
-public class Lp {
+public class Lp 
+{
 
-	public static void main(String[] args) {
-		
-		Scanner sc=new Scanner(System.in);
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
 System.out.println("Enter How Many Values To Calculate Cube :");
 int n=Integer.parseInt(sc.nextLine());
 for(int i=1;i<=n;i++)
 {
-	System.out.print("Number is "+i);
-	System.out.print(" And Cube Of The "+i);
-	
-	System.out.println(" Is : "+i*i*i);
+System.out.print("Number is "+i);
+System.out.print(" And Cube Of The "+i);
+System.out.println(" Is : "+i*i*i);
 }
-
-	}
+}
 
 }
