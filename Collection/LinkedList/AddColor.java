9d49de0bbecc79863d8023ef0eac936ab99f200b
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-07-24 09:53:06 +0530

    Add files via upload

---
+++ Collection/LinkedList/AddColor.java
@@ -0,0 +1,22 @@
+package linkedlist;
+
+import java.util.LinkedList;
+
+public class AddColor {
+	public static void main(String[] args) {
+		LinkedList<String>a=new LinkedList<String>();
+	    a.add("Red");
+	    a.add("Green");
+		a.add("Black");
+		a.add("White");
+		a.add("Pink");
+		System.out.println("Original linked list: ");
+		System.out.println(a);
+        a.add(1,"Yellow");
+		System.out.println("The Linked List: ");
+		System.out.println(a);
+
+
+	}
+
+}
