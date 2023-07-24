526105347f10fabc61611af6ed0649d313fe4515
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-07-24 09:53:40 +0530

    Add files via upload

---
+++ Collection/LinkedList/Append.java
@@ -0,0 +1,16 @@
+package linkedlist;
+import java.util.LinkedList;
+
+public class Append {
+
+	public static void main(String[] args) {
+	LinkedList<String>a=new LinkedList<String>();
+    a.add("Red");
+    a.add("Green");
+	a.add("Black");
+	a.add("White");
+	a.add("Pink");
+	a.add("Yellow");
+	System.out.println("The Linked List: "+a);
+	}
+}
