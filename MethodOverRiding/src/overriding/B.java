9e952625ef2fac7d4e9f1777ff8a4b1aec86d5f0
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-07-25 21:42:47 +0530

    Update B.java

--- MethodOverRiding/src/overriding/B.java
+++ MethodOverRiding/src/overriding/B.java
@@ -3,13 +3,11 @@ package overriding;
 public class B extends A{
 void operations(int b,int c)
 {
-	a=b*c;
-	System.out.println(a);
+a=b*c;
+System.out.println(a);
 }
-	public static void main(String[] args) {
-		
+public static void main(String[] args) {		
 B a=new B();
 a.operations(6, 5);
-	}
-
+}
 }
