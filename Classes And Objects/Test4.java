d269376ccbe6aef091e197cc31ba07f3c984e578
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-07-26 22:19:31 +0530

    Update Test4.java

--- Classes And Objects/Test4.java
+++ Classes And Objects/Test4.java
@@ -1,6 +1,7 @@
 package classes;
 public class Test4 {
 	public static void main(String[] args) {
+		
 		Class4 c=new Class4(4,5);
 		Class4 c1=new Class4(5,8);
 		System.out.println("Area : "+c.Area());
