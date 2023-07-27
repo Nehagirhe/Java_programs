f13d36b67588b4f566d342f05ba284f8cf265b5c
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-07-27 13:08:20 +0530

    Add files via upload

---
+++ Collection/LinkedList/ColorPos.java
@@ -0,0 +1,17 @@
+package linkedlist;
+
+import java.util.LinkedList;
+
+public class ColorPos {
+	public static void main(String[] args) {
+		LinkedList<String>a=new LinkedList<String>();
+	    a.add("Red");
+	    a.add("Green");
+		a.add("Black");
+		a.add("Pink");
+		a.add("Orange");
+		System.out.println("Original linked list: "+a);
+        
+       
+	}
+}
