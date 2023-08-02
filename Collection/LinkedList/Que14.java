334e97324df22e5de69e06a549805b683e4f9648
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-08-02 20:32:44 +0530

    Add files via upload

---
+++ Collection/LinkedList/Que14.java
@@ -0,0 +1,19 @@
+package linkedlist;
+
+import java.util.LinkedList;
+
+public class Que14 {
+
+	public static void main(String[] args) 
+	{
+		LinkedList<String>a=new LinkedList<String>();
+	    a.add("Red");
+	    a.add("Green");
+	    a.add(2,"Black");
+	    a.add(2,"Orange");
+	    System.out.println("Original linked list: "+a);
+        a.removeAll(a);
+        System.out.println("The New Linked List: "+a);
+	}
+
+}
