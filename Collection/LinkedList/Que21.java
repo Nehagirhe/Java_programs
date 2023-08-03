481548cc0cd03afe579af391e02ea767ca880edb
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-08-03 08:36:22 +0530

    Add files via upload

---
+++ Collection/LinkedList/Que21.java
@@ -0,0 +1,19 @@
+package linkedlist;
+
+import java.util.LinkedList;
+
+public class Que21 {
+
+	public static void main(String[] args) {
+		LinkedList<String>a=new LinkedList<String>();
+	    a.add("Red");
+	    a.add("Green");
+		a.add("Black");
+		a.add("White");
+		a.add("Pink");
+		System.out.println("Original Linked List: "+a);
+		System.out.println("Last Element In The List: "+a.getLast());
+		System.out.println("Original Linked List: "+a);
+	}
+
+}
