c531510d630204f3085fdc58dbaf370dd087329d
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-08-09 08:22:40 +0530

    Add files via upload

---
+++ Collection/LinkedList/Retrieve.java
@@ -0,0 +1,21 @@
+package linkedlist;
+
+import java.util.LinkedList;
+
+public class Retrieve {
+
+	public static void main(String[] args) {
+		LinkedList<String>a=new LinkedList<String>();
+	    a.add("Red");
+	    a.add("Green");
+		a.add("Black");
+		a.add("White");
+		a.add("Pink");
+		System.out.println("Original linked List: "+a);
+		 System.out.println("First Element In The List: "+a.get(0));
+        System.out.println("Original linked List: "+a);
+
+
+	}
+
+}
