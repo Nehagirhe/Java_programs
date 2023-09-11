92bab3583c7fd255085f56f441d95f1e64ae88d8
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-09-11 08:40:33 +0530

    Add files via upload

---
+++ Collection/map/TreeMap/Prog_5.java
@@ -0,0 +1,16 @@
+package Tree;
+
+import java.util.TreeMap;
+
+public class Prog_5 {
+
+	public static void main(String[] args) {
+		TreeMap<String, String>t=new TreeMap<String, String>();
+		t.put("c1", "Red");
+		t.put("c2", "Green");
+		t.put("c3", "Black");
+		t.put("c4", "White");
+		System.out.println("Output: "+t.keySet());
+	}
+
+}
