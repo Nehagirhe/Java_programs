9c43492035e90a0bede2f38a812a03145ef768b4
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-09-26 12:07:11 +0530

    Add files via upload

---
+++ Collection/map/TreeMap/Prog_18.java
@@ -0,0 +1,21 @@
+package Tree;
+
+import java.util.TreeMap;
+
+public class Prog_18 {
+
+	public static void main(String[] args) {
+		TreeMap<Integer,String>t=new TreeMap<Integer,String>();
+		t.put(10, "Red");
+		t.put(20, "Green");
+		t.put(40, "Black");
+		t.put(50, "White");
+		t.put(60, "Pink");
+		System.out.println("Sample Output: ");
+		System.out.println("Value Before Poll: "+t);
+		System.out.println("Value Returned: "+t.pollLastEntry());
+		System.out.println("Value After Poll: "+t);
+
+	}
+
+}
