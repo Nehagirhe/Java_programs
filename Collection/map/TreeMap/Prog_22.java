179cce965cb5e92c5a9ea2c0cf26ccf7f24f85ab
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-10-01 08:28:44 +0530

    Add files via upload

---
+++ Collection/map/TreeMap/Prog_22.java
@@ -0,0 +1,17 @@
+package Tree;
+import java.util.TreeMap;
+
+public class Prog_22 
+{
+	public static void main(String[] args) {
+	TreeMap<Integer,String>t=new TreeMap<Integer,String>();
+	t.put(10, "Red");
+	t.put(20, "Green");
+	t.put(30, "Black");
+	t.put(40, "White");
+	t.put(50, "Pink");
+	System.out.println("Sample Output: ");
+	System.out.println("\nOriginal TreeMap Content: "+t);
+   System.out.println("\nKeys Are Greater Than 20: "+t.tailMap(20, false));
+}
+}
