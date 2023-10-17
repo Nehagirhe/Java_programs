2944821d9c381b972069b9e80de20a9f5d0685f8
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-10-17 03:57:08 -0700

    Add files via upload

---
+++ Collection/TreeMap/Prog_8.java
@@ -0,0 +1,17 @@
+package Tree;
+
+import java.util.TreeMap;
+
+public class Prog_8 {
+
+	public static void main(String[] args) {
+		TreeMap<String,String>t=new TreeMap<String,String>();
+	    t.put("c1", "Red");
+	    t.put("c2", "Green");
+	    t.put("c3", "Black");
+	    t.put("c4", "White");
+	    System.out.println("Original TreeMap Content: "+t);
+	    System.out.println("Greatest Key: "+t.firstEntry());
+	    System.out.println("Least Key: "+t.lastEntry());
+	}
+}
