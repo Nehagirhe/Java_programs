162462b58d1a4af9e13c3fb56c9fdf91e77d9d4d
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-11-08 08:25:00 +0530

    Update Add.java

--- Collection/TreeSet/Add.java
+++ Collection/TreeSet/Add.java
@@ -1,1 +1,17 @@
+package Tree;
+import java.util.TreeSet;
+public class Add {
+
+	public static void main(String[] args) {
+	TreeSet<String>t=new TreeSet<String>();
+	t.add("Black");
+	t.add("Green");
+	t.add("Orange");
+	t.add("Red");
+	t.add("White");
+	System.out.println(t);
+	System.out.println("\nTree Set: \n"+t);
+	}
+
+}
 
