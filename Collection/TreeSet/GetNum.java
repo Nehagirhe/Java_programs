78b2e7026b2d82832d5131097c726734bb59f0a6
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-11-17 10:26:25 +0530

    Add files via upload

---
+++ Collection/TreeSet/GetNum.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class GetNum {
+
+	public static void main(String[] args)
+	{
+		HashSet<String>h=new HashSet<String>();
+	    h.add("Red");
+	    h.add("White");
+	    h.add("Pink");
+	    h.add("Yellow");
+	    h.add("Black");
+	    h.add("Green");
+	    System.out.println("The Hash Set: "+h);
+	    System.out.println("Size Of The Hash Set: "+h.size());
+
+	}
+
+}
