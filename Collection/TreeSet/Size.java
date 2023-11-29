63905a0a030a1ce24731036ce96075bd0cdf418e
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-11-29 10:48:30 +0530

    Add files via upload

---
+++ Collection/TreeSet/Size.java
@@ -0,0 +1,18 @@
+package Tree;
+
+import java.util.TreeSet;
+
+public class Size {
+
+	public static void main(String[] args) {
+		TreeSet<String>t=new TreeSet<String>();
+		t.add("Black");
+		t.add("Green");
+		t.add("Pink");
+		t.add("Red");
+		t.add("Orange");
+		System.out.println("Original Tree Set: "+t);
+		System.out.println("\nSize Of The Tree Set: "+t.size());
+	}
+
+}
