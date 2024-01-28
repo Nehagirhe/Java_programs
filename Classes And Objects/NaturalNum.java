869e65fff0650a39a41a42110e3bb1d4b90d95c4
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2024-01-28 12:34:56 +0530

    Add files via upload

---
+++ Classes And Objects/NaturalNum.java
@@ -0,0 +1,13 @@
+//Program To Print The Sum Of First 10 Natural Number
+public class NaturalNum {
+	
+	public static void main(String[] args) {
+		int sum1=0;
+		for(int i=1;i<=10;i++){
+			System.out.println(i);
+			sum1=sum1+i;
+		}
+		System.out.println("Sum Of First 10 Ntural Number Is : "+sum1);
+	}
+
+}
